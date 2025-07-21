d343a292342e042f538ac32c59415f70310f0fb5
Kremilly <contact@kremilly.com>
2025-07-21 11:19:54 -0300

    fix: update redirect URL from 'httpshield.net' to 'nexvul.com'

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "https://httpshield.net", http.StatusFound)
+		http.Redirect(w, r, "https://nexvul.com", http.StatusFound)
 	})
 
 	http.HandleFunc("/get", core.HandleScreenshot)
